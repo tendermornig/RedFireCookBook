3109442beba2923514081f1fd2f17b0466fa3788
陈龙 <2802660864@qq.com>
2020-10-27 21:04:37 +0800

    建错东西了，将添加日记页面由碎片改为活动。

---
+++ app/src/main/java/com/hnqcgc/redfirecookbook/ui/adddiary/AddDiaryActivity.java
@@ -0,0 +1,16 @@
+package com.hnqcgc.redfirecookbook.ui.adddiary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hnqcgc.redfirecookbook.R;
+
+public class AddDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diary);
+    }
+}
