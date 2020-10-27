35eb62a39f0b71eacff55a746d39bb1bdaf09eec
陈龙 <2802660864@qq.com>
2020-10-27 11:44:03 +0800

    添加了底部导航栏厨房日记页面选项

---
+++ app/src/main/java/com/hnqcgc/redfirecookbook/ui/kitchendiary/KitchenDiaryFragment.java
@@ -0,0 +1,6 @@
+package com.hnqcgc.redfirecookbook.ui.kitchendiary;
+
+import androidx.fragment.app.Fragment;
+
+public class KitchenDiaryFragment extends Fragment {
+}
---
+++ app/src/main/java/com/hnqcgc/redfirecookbook/ui/kitchendiary/KitchenDiaryViewModel.java
@@ -0,0 +1,7 @@
+package com.hnqcgc.redfirecookbook.ui.kitchendiary;
+
+import androidx.lifecycle.ViewModel;
+
+public class KitchenDiaryViewModel extends ViewModel {
+
+}
